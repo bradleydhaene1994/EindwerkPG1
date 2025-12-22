1b3e2f521cd24d13013b0d38ad36813c1f9e5d9b
bradleydhaene1994 <204046398+bradleydhaene1994@users.noreply.github.com>
2025-12-22 12:00:38 +0100

    Exceptions Added

---
+++ Programmeren Gevorderd 1 Eindwerk/Eindwerk PG1/CustomerSimulationBL/Exceptions/AddressException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerSimulationBL.Exceptions
+{
+    public class AddressException : Exception
+    {
+        public AddressException()
+        {
+        }
+        public AddressException(string? message) : base(message)
+        {
+        }
+        public AddressException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
