6548c304dab2ea1a703c503748d8c51f8c67c36c
bradleydhaene1994 <204046398+bradleydhaene1994@users.noreply.github.com>
2025-12-26 15:51:08 +0100

    Update

--- Programmeren Gevorderd 1 Eindwerk/Eindwerk PG1/CustomerSimulationBL/DTOs/CustomerDTO.cs
+++ Programmeren Gevorderd 1 Eindwerk/Eindwerk PG1/CustomerSimulationBL/DTOs/CustomerDTO.cs
@@ -8,5 +8,25 @@ namespace CustomerSimulationBL.DTOs
 {
     public class CustomerDTO
     {
+        public CustomerDTO(int id, string name, string firstName, string lastName, string municipality, string street, DateTime birthDate, string houseNumber)
+        {
+            Id = id;
+            Name = name;
+            FirstName = firstName;
+            LastName = lastName;
+            Municipality = municipality;
+            Street = street;
+            BirthDate = birthDate;
+            HouseNumber = houseNumber;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Municipality { get; set; }
+        public string Street { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string HouseNumber { get; set; }
     }
 }
--- Programmeren Gevorderd 1 Eindwerk/Eindwerk PG1/CustomerSimulationBL/DTOs/SimulationSettingsDTO.cs
+++ Programmeren Gevorderd 1 Eindwerk/Eindwerk PG1/CustomerSimulationBL/DTOs/SimulationSettingsDTO.cs
@@ -8,13 +8,21 @@ namespace CustomerSimulationBL.DTOs
 {
     public class SimulationSettingsDTO
     {
+        public SimulationSettingsDTO(int id, List<int> selectedMunicipalityIds, int totalCustomers, int minAge, int maxAge, string houseNumberRules)
+        {
+            Id = id;
+            SelectedMunicipalityIds = selectedMunicipalityIds;
+            TotalCustomers = totalCustomers;
+            MinAge = minAge;
+            MaxAge = maxAge;
+            HouseNumberRules = houseNumberRules;
+        }
+
+        public int Id {  get; set; }
         public List<int> SelectedMunicipalityIds { get; set; } = new();
         public int TotalCustomers { get; set; }
         public int MinAge { get; set; }
         public int MaxAge { get; set; }
-        public int MinHouseNumber { get; set; }
-        public int MaxHouseNumber { get; set; }
-        public bool HasLetters { get; set; }
-        public int LetterPercentage { get; set; }
+        public string HouseNumberRules { get; set; }
     }
 }
